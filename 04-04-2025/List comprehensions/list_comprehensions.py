9575f05c02c01e24f0a821a9ac921a23bef9a6f4
surajjoshi7490 <happywaves06@gmail.com>
2025-04-04 17:16:24 +0530

    comit

---
+++ 04-04-2025/List comprehensions/list_comprehensions.py
@@ -0,0 +1,15 @@
+# what we do 
+'''
+my_string="SURAJ"
+for i in my_string:
+    print(i)        ''' 
+
+# what we can do 
+'''
+my_string="SURAj"
+new_string=[ i for i in  my_string ]
+print(new_string) '''
+
+# what else
+new_string=[ i for i in "words"]
+print(new_string)
