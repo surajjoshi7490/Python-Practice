75b9946492c1fe13282ca59994744189634c3f13
surajjoshi7490 <happywaves06@gmail.com>
2025-04-04 16:33:57 +0530

    ok

--- 04-04-2025/Conditional projects/Agegroup.py
+++ 04-04-2025/Conditional projects/Agegroup.py
@@ -2,8 +2,11 @@
 #  child < 13 , Teenager 13 - 19 , Adult 20 - 59 , Senior 60+
 
 age = int(input("Enter your age "))
+
 if age < 13 :
+    print("You are a child")
 elif 13<=age<=19 :
+    print("Your are a Teenager")
 elif 20<=age<=59:
     print("You are a Adult ")
 elif age >=60:
